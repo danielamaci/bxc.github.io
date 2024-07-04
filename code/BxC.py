5c8fb75067c306f3fe0fd0b4976c2116e03a722a
danielamaci <daniela.maci97@gmail.com>
2024-07-04 16:46:38 +0200

    typos and minor modifications

--- code/BxC.py
+++ code/BxC.py
@@ -2,10 +2,12 @@
 The script generates turbulent magnetic field and current density field
 It distinguishes between purely turbulent and structured magnetic field
 
-Choose the parameters in the parameters.py file before rnning the script 
+Choose the parameters in the parameters.py file before running the script 
 """
 from parameters import *
 from fields_functions import *
+
+print('Generating B field at resolution ' + str(N) + 'and background topolgy ' + str(B0_model))
 
 if B0_model == 'none':  ## Build the purely turbulent magnetic and current density field
     alpha = 1  # == no filtering
@@ -16,7 +18,8 @@ if B0_model == 'none':  ## Build the purely turbulent magnetic and current densi
     B = build_B(S, FGF, alpha)
     B = B/(np.amax(np.sqrt(B[0]**2 + B[1]**2 + B[2]**2)))
     
-    j = build_j(B) # Already normalized inside the function build_j
+    del(r, FGF, S) ## comment this line if you want to see intermediate steps
+    # j = build_j(B) # Uncomment if you also need the current density field
 else: ## Build turbulent magnetic field with background structure
     B0 = build_B0(B0_model)
     B0 = B0/(np.amax(np.sqrt(B0[0]**2 + B0[1]**2 + B0[2]**2)))
@@ -30,15 +33,18 @@ else: ## Build turbulent magnetic field with background structure
     Bturb = Bturb/(np.amax(np.sqrt(Bturb[0]**2 + Bturb[1]**2 + Bturb[2]**2)))
     
     ## At this point, these fields could be deleted to save memory
-    # del(r, FGF, S)
+    del(r, FGF, S) ## comment this line if you want to see intermediate steps
     
-    jturb = build_j(Bturb) #comment if not needed
+    # jturb = build_j(Bturb) # Uncomment if you also need the current density field of the purely turbulent field
     
     gamma = 0.3 #constant filtering 
     
     B = B0 + gamma*Bturb
     
     ## At this point, this field could be deleted to save memory
-    # del(Bturb) 
+    del(Bturb) # Comment if you need the purely turbulent field
     
-    j = build_j(B)    
+    # j = build_j(B)  # Uncomment if you also need the current density field of the total turbulent field
+
+print('Saving the Bfield in root folder')
+np.savez('./B.npz', B=B)
